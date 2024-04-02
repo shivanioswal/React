37042dca9a2661d313b83d430efa70054aa51a00
Shivani Oswal <shivanimoswal@gmail.com>
2024-04-02 19:42:03 +0530

    Class Component Practice code

---
+++ class-component/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component{
+  render()
+  {
+    return(
+      <h1>Hello I am a class component</h1>
+    )
+  }
+}
+
+export default App
